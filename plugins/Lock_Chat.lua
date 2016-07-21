f056363f7cb7c7223d66a2d1d1e85f5dc82c5b94
p00ria <amir1300sobatdar@gmail.com>
2016-07-21 18:19:17 +0430

    Create Lock_Chat.lua

---
+++ plugins/Lock_Chat.lua
@@ -0,0 +1,26 @@
+local function run(msg, matches)
+    if is_momod(msg) then
+        return
+    end
+    local data = load_data(_config.moderation.data)
+    if data[tostring(msg.to.id)] then
+        if data[tostring(msg.to.id)]['settings'] then
+            if data[tostring(msg.to.id)]['settings']['chat'] then
+                lock_chat = data[tostring(msg.to.id)]['settings']['chat']
+            end
+        end
+    end
+    local chat = get_receiver(msg)
+    local user = "user#id"..msg.from.id
+    if lock_chat == "yes" then
+       delete_msg(msg.id, ok_cb, true)
+    end
+end
+ 
+return {
+  patterns = {
+"^(.*)$"
+  },
+  run = run
+}
+
