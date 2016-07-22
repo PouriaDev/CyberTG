ca8f4c146bd0359890f56ae4d1e88542c5c209c4
p00ria <amir1300sobatdar@gmail.com>
2016-07-22 20:58:35 +0430

    Update App.lua

--- plugins/App.lua
+++ plugins/App.lua
@@ -1,47 +1,56 @@
-do
-local function run(msg, matches)
-	if matches[1]:lower() == 'app' then
-		local url = http.request('http://api.magic-team.ir/plazza/search.php?key='..URL.escape(matches[2]))
-		local jdat = json:decode(url)
-		items = jdat
-		local text = ''
-		local msgss = 0
-		for item in pairs(items) do
-		msgss = msgss + 1
-		text = text..msgss..' 📦 عنوان: '..items[msgss].title..' 🔰   Package Id: '..items[msgss].pack..'\n\n'
-    local hash = 'app:'..msg.from.id..msgss
-	local hash1 = 'img:'..msg.from.id..msgss
-  redis:set(hash, items[msgss].pack)
-  redis:set(hash1, items[msgss].icon)
-		
-		end
-		return text..'برای دریافت اطلاعات برنامه از دستور زیر استفاده کنید\n/appinfo number\n(example): /appinfo 1'
-	end
-	if matches[1]:lower() == 'appinfo' then
-	local hashz = 'app:'..msg.from.id..matches[2]
-	local hash1z = 'img:'..msg.from.id..matches[2]
-	pp = redis:get(hashz)
-	pp1 = redis:get(hash1z)
-	local url = http.request('http://api.magic-team.ir/plazza/info.php?key='..pp)
-		local jdat = json:decode(url)
-		if jdat.needroot == "false" then 
-		root = 'خیر'
-		else
-		root = 'بله'
-		end
-		ple = math.floor(jdat.dlsize / 1024)
-		text = 'عنوان: \n'..jdat.title..'\nنام پکیج :\n'..pp..'\nدرباره: \n'..jdat.info..'\nورژن: \n'..jdat.version..'\nنیاز به روت : '..root..'\nسایز : '..ple..'\n تصویر : '..pp1..'\nلینک دانلود : '..jdat.dlurl
-		return text
-	end
+function shortlink(url)
+return http.request('http://gs2.ir/api.php?url='..url)
 end
-
+function run(msg, matches)
+  local hash = 'app:'..msg.to.id
+  local hash2 = 'appicon:'..msg.to.id
+  local hash3 = 'appcreator:'..msg.to.id
+  if matches[1]:lower() == "appinfo" then
+    local value = redis:hget(hash,matches[2])
+  local photo = redis:hget(hash2,matches[2])
+  local creator = redis:hget(hash3,matches[2])
+    if not value then
+      return 'نرم افزار مورد نظر یافت نشد.'
+    else
+  local data = http.request('http://api.magic-team.ir/plazza/info.php?key='..value)
+  local db = json:decode(data)
+  local needroot = 'خیر'
+  local nroot = db.needroot
+  if nroot then
+  needroot = 'بله'
+  end
+  local size = db.size
+  local exp = db.size.." / 1000000"
+  local sizeurl = 'http://api.mathjs.org/v1/?expr='..URL.escape(exp)
+    local b,c = http.request(sizeurl)
+    local text = "عنوان : \n<code>"..db.title.."</code>\n نام پکیج :‌\n"..value.."\nسازنده :\n"..creator.."\n درباره : \n"..db.info.."\n ورژن : \n"..db.version.."\n نیاز به روت : \n"..needroot.."\n سایز : \n"..math.floor(b).." mb\n تصویر :\n"..photo.."\n لینک دانلود :\n"..shortlink(db.dlurl)
+  send_large_msg(get_receiver(msg),text,ok_cb,false)
+    return 
+    end
+    return
+  elseif matches[1]:lower() == 'app' then
+  local url = http.request('http://api.magic-team.ir/plazza/search.php?key='..URL.escape(matches[2]))
+  local jdat = json:decode(url)
+  local text = "نتایج برای "..URL.escape(matches[2]).." : \n"
+  redis:del(hash)
+  if #jdat < 1 then return "نرم افزار مورد نظر یافت نشد." end
+    for i = 1, #jdat do
+      text = text..i..'- 📦 '..jdat[i].title..'\n🔰 سازنده : '..(jdat[i].subtitle or 'نامشخص')..'\n\n'
+    local l = shortlink(jdat[i].icon)
+      redis:hset(hash,i,jdat[i].pack)
+      redis:hset(hash2,i,l)
+    redis:hset(hash3,i,""..(jdat[i].subtitle or 'نامشخص').."")
+    end
+    text = text..'برای دریافت اطلاعات برنامه از دستور زیر استفاده کنید\n/appinfo number\n(example): /appinfo 1'
+  return text
+end
+end
 return {
-   patterns = {
-"^[/!#]([Aa]pp) (.*)$",
-"^[/!#]([Aa]ppinfo) (.*)$",
-"^([Aa]pp) (.*)$",
-"^([Aa]ppinfo) (.*)$",
-   },
-   run = run
+patterns = {
+  "^[!/#]([Aa][Pp][Pp]) (.*)$",
+  "^[!/#]([Aa][Pp][Pp][Ii][Nn][Ff][Oo]) (%d+)$",
+  "^([Aa][Pp][Pp]) (.*)$",
+  "^([Aa][Pp][Pp][Ii][Nn][Ff][Oo]) (%d+)$"
+  }, 
+  run = run 
 }
-end
