c3dfe5b8daddeda506b63832c2937df8feca8c39
p00ria <amir1300sobatdar@gmail.com>
2016-07-22 20:17:55 +0430

    Create Time.lua

---
+++ plugins/Time.lua
@@ -0,0 +1,16 @@
+local function run(msg, matches)
+local url , res = http.request('http://api.gpmod.ir/time/')
+local jdat = json:decode(url)
+local url = "http://latex.codecogs.com/png.download?".."\\dpi{500}%10\\LARGE%75"..jdat.ENtime
+local file = download_to_file(url,'signal.webp')
+send_document(get_receiver(msg) , file, ok_cb, false)
+end
+ 
+return {
+  patterns = {
+    "^[!/#]([Tt][Ii][Mm][Ee])$",
+    "^([Tt][Ii][Mm][Ee])$"
+  }, 
+  run = run 
+}
+
