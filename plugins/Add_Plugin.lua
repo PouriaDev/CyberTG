c66959a20f49882d4f123f405d99de6ddf6a2261
p00ria <amir1300sobatdar@gmail.com>
2016-07-22 14:46:25 +0430

    Create Add_Plugin.lua

---
+++ plugins/Add_Plugin.lua
@@ -0,0 +1,28 @@
+do
+ local function save_file(name, text)
+    local file = io.open("./plugins/"..name, "w")
+    file:write(text)
+    file:flush()
+    file:close()
+    return "فایل ارسالی در سرور ذخیره شد"
+end   
+function run(msg, matches)
+  if matches[1] == "addplugin" and is_sudo(msg) then
+ 
+         local name = matches[2]
+        local text = matches[3]
+        return save_file(name, text)
+        end
+        if not is_sudo(msg) then 
+		return "شما دسترسی ندارید"
+	end
+end
+return {
+  patterns = {
+  "^[!/#]([Aa]ddplugin) ([^%s]+) (.+)$",
+  "^([Aa]ddplugin) ([^%s]+) (.+)$"
+  },
+  run = run
+}
+end
+
