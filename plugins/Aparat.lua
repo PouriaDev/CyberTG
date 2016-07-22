5e885609c10f930210fd24f568c89106f5521c0d
p00ria <amir1300sobatdar@gmail.com>
2016-07-22 15:01:42 +0430

    Update Aparat.lua

--- plugins/Aparat.lua
+++ plugins/Aparat.lua
@@ -8,7 +8,7 @@ local function run(msg, matches)
   for i = 1, #items do
   text = text..'\n\n'..i..'-'..items[i].title..'\nتعداد بازدید: '..items[i].visit_cnt..'\n   لینک: aparat.com/v/'..items[i].uid
   end
-  text = text..'\n\n@CyberTM'
+  text = text..''
   return text
  end
 end
