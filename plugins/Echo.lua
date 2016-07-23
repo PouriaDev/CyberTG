17f0b3bca8370887e52437e26ddf066e5a5e510f
p00ria <amir1300sobatdar@gmail.com>
2016-07-23 12:38:27 +0430

    Create Echo.lua

---
+++ plugins/Echo.lua
@@ -0,0 +1,28 @@
+
+local function run(msg, matches)
+  local text = matches[1]
+  local b = 1
+
+  while b ~= 0 do
+    text = text:trim()
+    text,b = text:gsub('^!+','')
+    text,b = text:gsub('^/+','')
+    text,b = text:gsub('^#+','')
+  end
+  if is_momod(msg) then
+  return text
+else
+  return 'Only Moderators Can!'
+end
+end
+return {
+  description = "Simplest plugin ever!",
+  usage = "/echo [whatever]: echoes the msg",
+  patterns = {
+    "^[!/#]([Ee]cho) +(.+)$",
+    "^([Ee]cho) +(.+)$",
+	"^بگو +(.+)$",
+  }, 
+  run = run 
+}
+
