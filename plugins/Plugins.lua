b3d571ec08a3d6573ee478b1248791fb8f2ca58e
p00ria <amir1300sobatdar@gmail.com>
2016-07-26 12:16:21 +0430

    Update Plugins.lua

--- plugins/Plugins.lua
+++ plugins/Plugins.lua
@@ -152,7 +152,7 @@ end
 
 local function run(msg, matches)
   -- Show the available plugins 
-  if matches[1] == 'plist' and is_sudo(msg) then --after changed to moderator mode, set only sudo
+  if matches[1] == '!plist' and is_sudo(msg) then --after changed to moderator mode, set only sudo
     return list_all_plugins()
   end
 
